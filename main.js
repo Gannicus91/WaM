b6744f0076b8d00fdb2e04fe9eb6c2adfd16f585
Maxim Givanyan <givanyan99@yandex-team.ru>
2022-07-04 22:03:15 +0300

    remove main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-const {userExists} = require('./helpers')
-
-const userData = JSON.parse(String(fs.readFileSync('member_data.json'))).map(el => ({
-	...el,
-	nameComponents: el.nameComponents,
-}))
-
-console.log(userExists('ильчук', userData));
