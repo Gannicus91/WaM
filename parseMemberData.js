1c6f3d0fa7b72d7e2568a41061feed0883ed40f6
Maxim Givanyan <givanyan99@yandex-team.ru>
2022-07-04 22:06:33 +0300

    update parseMemberData.js

--- parseMemberData.js
+++ parseMemberData.js
@@ -1,16 +1,25 @@
 const fs = require('fs');
+const _ = require('lodash');
+const { userExists } = require('./helpers');
+const { toJSON } = require('./utils');
 
-const data = String(fs.readFileSync('members.txt'));
+const data = String(fs.readFileSync('data.csv'));
 
-const formattedData = data.split('\n').reduce((prev, el, idx) => {
-	if (!el) {
+const formattedData = data.split('\r\n').reduce((prev, el, idx) => {
+	if (!el || idx === 0) {
 		return prev;
 	}
 
+	const [name, secret, dateOfBirth, city, image] = el.split(',');
+
 	prev.push({
-		id: idx + 1,
-		name: el,
-		nameComponents: el.split(/\s+/gm).reduce((prev, el) => {
+		id: idx,
+		name: name.trim(),
+		image: image || null,
+		dateOfBirth,
+		city,
+		secret_code: secret,
+		nameComponents: name.split(/\s+/gm).reduce((prev, el) => {
 			if (!el) {
 				return prev;
 			}
@@ -18,18 +27,28 @@ const formattedData = data.split('\n').reduce((prev, el, idx) => {
 			prev.push(el.toLowerCase());
 
 			return prev;
-		}, [])
+		}, []),
 	})
 
 	return prev;
 }, []);
 
-const jsonData = JSON.stringify(formattedData, (k, v) => {
-	if (v instanceof Set) {
-		return [...v];
-	}
+const guides = formattedData.reduce((prev, el, idx, arr) => {
+	const other = [...arr];
+	other.splice(idx, 1);
+	prev[el.name] = _.shuffle(other.map(el => el.name));
 
-	return v;
-}, 2);
+	return prev;
+}, {})
 
-fs.writeFileSync('member_data.json', jsonData);
+function generateGuidesId(guides) {
+	return Object.keys(guides).reduce((prev, el) => {
+		const user = userExists(el, formattedData);
+		prev[user.id] = guides[el].map(name => userExists(name, formattedData).id);
+		return prev
+	}, {});
+}
+
+fs.writeFileSync('member_data.json', toJSON(formattedData));
+fs.writeFileSync('member_guides.json', toJSON(guides));
+fs.writeFileSync('guides.json', toJSON(generateGuidesId(guides)));
